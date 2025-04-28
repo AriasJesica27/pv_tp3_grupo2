c9c9ab1877af8524b6466bd3d4fef950dffae47a
Arias Yesica <jesicaarias271@gmail.com>
2025-04-28 00:50:42 -0300

    mostrar lista de tareas usando  map

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+import TaskInput from "./TaskInput"; // Importa TaskInput que está en otro archivo
+
 function App() {
-  const [texto,setTexto]= useState("");
+  const [texto, setTexto] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+  const handleSubmit = () => {
+    if (texto.trim() !== "") {
+      setTareas([...tareas, texto]);
+      setTexto(""); // limpia el input
+    }
+  };
+
   return (
     <div>
       <h1>Lista de tareas</h1>
-      <TaskInput elemento={[texto,setTexto]}></TaskInput>
-        
+
+      <TaskInput 
+        texto={texto} 
+        setTexto={setTexto} 
+        handleSubmit={handleSubmit} 
+      />
+
+      <ul>
+        {tareas.map((tarea, index) => (
+          <li key={index}>{tarea}</li>
+        ))}
+      </ul>
     </div>
   );
 }
